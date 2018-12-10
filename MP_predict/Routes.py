e35f28c37c181bf4ff6bb5e98f99362ad54fc200
RealDinosaur <novemberhascome2@gmail.com>
2018-12-09 19:04:53 -0500

    Update Routes.py
    Added code to create dict array

--- MP_predict/Routes.py
+++ MP_predict/Routes.py
@@ -1,5 +1,5 @@
 '''
-Routes class takes a given area and creates a json object for that entire area. 
+Routes class takes a given area and creates a json object for that entire area.
 Eventually, individual routes will be parsed out and saved into an array.
 '''
 import requests
@@ -8,8 +8,15 @@ import json
 class Routes():
 
     def __init__(self, route_url):
-        self.request =  requests.get(route_url)
-        self.json = self.request.json()
-        self.route_list = []
-        print(self.json)
-        
+        self.page =  requests.get(route_url)
+        self.data = json.loads(self.page.content.decode('utf-8'))
+        self.route_dict = self.data['routes']
+        self.getDescription
+
+        print(self.route_dict[5].get('id'))
+
+    def getDescription(self):
+        for route in route_dict:
+            # description =
+            # route['description'] = description
+
