79d5652b325d905372925e8cc66d54d24e4f8919
carleen <carleenboyer@gmail.com>
2018-12-09 21:53:40 -0500

    Added getDescription fcn to Routes class, added some comments

--- MP_predict/Routes.py
+++ MP_predict/Routes.py
@@ -2,8 +2,9 @@
 Routes class takes a given area and creates a json object for that entire area.
 Eventually, individual routes will be parsed out and saved into an array.
 '''
-import requests
 import json
+from lxml import html
+import requests
 
 class Routes():
 
@@ -11,12 +12,31 @@ class Routes():
         self.page =  requests.get(route_url)
         self.data = json.loads(self.page.content.decode('utf-8'))
         self.route_dict = self.data['routes']
-        self.getDescription
+        self.getDescription()
+        self.convertGradeToInt()
 
-        print(self.route_dict[5].get('id'))
+        #print(self.route_dict[5])
+        #print(self.route_dict[5].get('id'))
 
+    '''
+    Get URL from route dictonary, use HTML parser to get route description from
+    MP page, add value of "description" for the route. 
+
+    [[NOTE: had to add try/except, route "Baby With a Nail Gun" was showing error
+    at html_description, most likely an issue on MP site.]]
+    '''
     def getDescription(self):
-        for route in route_dict:
-            # description =
-            # route['description'] = description
+        i = 0
+        for route in self.route_dict:
+            i += 1
+            route_url = route['url'] 
+            page = requests.get(route_url)
+            tree = html.fromstring(page.content)
+            try:
+                html_description = tree.xpath('//*[@id="route-page"]/div/div[3]/div[1]/div[3]/div[2]/div/text()')
+                route['description'] = html_description[0]
+            except:
+                route['description'] = 'No description'
 
+    def convertGradeToInt(self):
+        pass
