b0999a7bfd2b80b33a9ec45dcb74628cf1d34fb7
carleen <carleenboyer@gmail.com>
2019-10-02 18:25:14 -0400

    Changed to pandas dataframe

--- MP_predict/test.py
+++ MP_predict/test.py
@@ -5,7 +5,7 @@ Testing file for classes
 from Routes import Routes
 
 def read_file():
-    url = 'https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=38.078&lon=-81.063&maxDistance=60&minDiff=5.6&maxDiff=5.15&key=200219818-f9fb243d0b8066ff66b9edc9f49bd3e4'
+    url = 'https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=38.078&lon=-81.063&maxDistance=60&key=200219818-f9fb243d0b8066ff66b9edc9f49bd3e4'
     area = 'NRG'
     route = Routes(url)
 
